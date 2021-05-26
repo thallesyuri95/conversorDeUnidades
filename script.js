fbd852b6a8d684a55573137a447004e08ad4cbe8
Thalles Yuri <thalles.yuri1995@gmail.com>
2021-05-26 08:03:30 -0700

    Adding others units to MyData

--- script.js
+++ script.js
@@ -1,62 +1,56 @@
-const [mg, g, kg, ton] = ['miligramas', 'gramas', 'quilogramas', 'toneladas'];
-const [cel, kel, fah] = ['celsius', 'kelvin', 'fahrenheit'];
-
-const converterMassa = (uniInicial, uniFinal, valor) => {
-  switch (uniInicial) {
-    case mg:
-      return {
-        gramas: `${valor * 1000} ${uniFinal}`,
-        quilogramas: `${valor * 100000} ${uniFinal}`,
-        toneladas: `${valor * 1000000} ${uniFinal}`,
-      }[uniFinal];
-      break;
-    case g:
-      return {
-        miligramas: `${valor * 1000} ${uniFinal}`,
-        quilogramas: `${valor / 1000} ${uniFinal}`,
-        toneladas: `${valor / 1000000} ${uniFinal}`,
-      }[uniFinal];
-      break;
-    case kg:
-      return {
-        miligramas: `${valor * 1000000} ${uniFinal}`,
-        gramas: `${valor * 1000} ${uniFinal}`,
-        toneladas: `${valor / 1000} ${uniFinal}`,
-      }[uniFinal];
-      break;
-    case ton:
-      return {
-        miligramas: `${valor / 100000000} ${uniFinal}`,
-        gramas: `${valor / 1000000} ${uniFinal}`,
-        quilogramas: `${valor / 1000} ${uniFinal}`,
-      }[uniFinal];
-      break;
-  }
+let myData = {
+  mass: {
+    mg: 1 / 1000, //1mg = 1/1000g,
+    g: 1, //1g = 1g,
+    kg: 1000, //1kg = 1000g,
+    ton: 1000000, //1ton = 1000000g
+  },
+  temperature: {
+    celsius: {
+      fahrenheit: (valor) => valor * 1.8 + 32,
+      kelvin: (valor) => valor + 273.15,
+    },
+    fahrenheit: {
+      celsius: (valor) => (valor - 32) / 1.8,
+      kelvin: (valor) => (valor - 32) / 1.8 + 273.15, // fahrenheit.celsius + 273.15
+    },
+    kelvin: {
+      celsius: (valor) => valor - 273.15,
+      fahrenheit: (valor) => (valor - 273.15) * 1.8 + 32, // kelvin.celsius * 1.8 + 32
+    },
+  },
+  length: {
+    mm: 1 / 1000,
+    cm: 1 / 100,
+    m: 1,
+    km: 1000,
+  },
+  time: {
+    s: 1,
+    min: 60,
+    h: 3600,
+    day: 86400,
+    week: 604800,
+    month: 2629600,
+  },
+  volume: {
+    m3: 1,
+    l: 1000,
+    dm3: 1000,
+    cm3: 1000000,
+  },
 };
+//Input: value
+//Input:
+//initialUnit-> Select[ton,kg,g,mg] to finalUnit-> Select[ton,kg,g,mg]
+let initialUnit = myData.temperature.celsius; //From HTML input
+let finalUnit = myData.temperature.kelvin.fahrenheit; //From HTML input
+// function converter(value) {
+//   console.log(value * (initialUnit / finalUnit));
+// }
 
-console.log(converterMassa(mg, ton, 5));
-
-const converterTemperatura = (uniInicial, uniFinal, valor) => {
-  switch (uniInicial) {
-    case cel:
-      return {
-        kelvin: `${valor + 273.15} ${uniFinal}`,
-        fahrenheit: `${valor * 1.8 + 32} ${uniFinal}`,
-      }[uniFinal];
-      break;
-    case kel:
-      return {
-        celsius: `${valor - 273.15} ${uniFinal}`,
-        fahrenheit: `${(valor - 273.15) * 1.8 + 32} ${uniFinal}`,
-      }[uniFinal];
-      break;
-    case fah:
-      return {
-        celsius: `${valor - 32 / 1.8} ${uniFinal}`,
-        kelvin: `${(valor - 32) * 1.8 + 273.15} ${uniFinal}`,
-      }[uniFinal];
-      break;
-  }
-};
-
-console.log(converterTemperatura(fah, cel, 64));
+function converterTemperatura(value) {
+  console.log(finalUnit(value));
+}
+converterTemperatura(283.15);
+// 500g*(1g/1g)*(1kg/1000g) = 0.5kg
